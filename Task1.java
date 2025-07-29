34ff46b51e88e233a34dc5d07c790260cdc02e2d
Erbol Zhaparov <ae.270718@gmail.com>
2025-07-30 01:19:45 +0600

    finished first task

--- Task1.java
+++ Task1.java
@@ -1,3 +1,12 @@
 public class Task1 {
     
+    public static void main(String[] args) {
+        
+        System.out.print("Введдите название футбольного клуба: ");
+        String clubName = System.console().readLine().strip();
+
+        String result = "Название: %s\nКол-во символов: %s\n%s\n%s";
+
+        System.out.printf(result, clubName, clubName.length(), clubName.toUpperCase(), clubName.toLowerCase());
+    }
 }
