8461f906b0f263f7e52905d3494ed0d89375c368
Erbol Zhaparov <ae.270718@gmail.com>
2025-07-30 02:03:29 +0600

    finished second task

--- Task1.java
+++ Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
         System.out.print("Введдите название футбольного клуба: ");
         String clubName = System.console().readLine().strip();
 
-        String result = "Название: %s\nКол-во символов: %s\n%s\n%s";
+        String result = "\nНазвание: %s\nКол-во символов: %s\n%s\n%s";
 
         System.out.printf(result, clubName, clubName.length(), clubName.toUpperCase(), clubName.toLowerCase());
     }
--- Task2.java
+++ Task2.java
@@ -1,3 +1,26 @@
 public class Task2 {
     
+    public static void main(String[] args) {
+
+        System.out.print("Введите превую фамилию: ");
+        String surname1 = System.console().readLine().strip();
+        System.out.print("Введите вторую фамилию: ");
+        String surname2 = System.console().readLine().strip();
+
+        String tmpl = "Фамилия %s %s чем %s\n";
+        String snt = "";
+
+        System.out.println("\nФамилия: " + surname1 + " | длина: " + surname1.length());
+        System.out.println("Фамилия: " + surname2 + " | длина: " + surname2.length() + "\n");
+
+        if (surname1.length() > surname2.length()) {
+            snt = "длиннее";
+        } else if (surname1.length() < surname2.length()) {
+            snt = "короче";
+        } else {
+            tmpl = "Фамилии равны";
+        }
+
+        System.out.printf(tmpl, surname1, snt, surname2);
+    }
 }
