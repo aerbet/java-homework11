7561aef5ea4bfe04e292e9f8bf06f8a94a3c184d
Erbol Zhaparov <ae.270718@gmail.com>
2025-07-31 02:26:46 +0600

    finished task 3 and 4

--- Task3.java
+++ Task3.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Task3 {
     
+    public static void main(String[] args) {
+        
+        System.out.print("Введите значение x: ");
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextDouble();
+        double y;
+
+        String tmpl = "Уравнение не решается: 0 != %s(%s)";
+
+        if (x < 0) {
+            y = 3 * x + 1;
+            System.out.println("y = " + y);
+        } else if (x == 0) {
+            System.out.printf(tmpl, "cos", Math.round(x));
+        } else {
+            if (x == Math.PI) {
+                System.out.println("Уравнение решено: 0 = sin(Math.PI)");
+            } else {
+                System.out.printf(tmpl, "sin", Math.round(x));
+            }
+        }
+
+        System.out.println();
+
+        sc.close();
+    }
 }
--- Task4.java
+++ Task4.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Task4 {
     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите скорость в км/час: ");
+        double kmHour = sc.nextDouble();
+
+        System.out.print("Введите скорость в м/сек: ");
+        double mSec = sc.nextDouble();
+
+        String tmpl = "\n%s %s больше чем %s %s\n";
+
+        if (mSec * 3.6 < kmHour) {
+            System.out.printf(tmpl, kmHour, "км/ч", mSec, "м/сек");
+        } else if (mSec * 3.6 > kmHour) {
+            System.out.printf(tmpl, mSec, "м/сек", kmHour, "км/ч");
+        } else if (mSec * 3.6 == kmHour) {
+            tmpl = "\nСкорости равны\n";
+            System.out.println(tmpl);
+        } 
+
+        sc.close();
+    }
 }
